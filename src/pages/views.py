4ab6e2da8e4643d1ae38363fc516712a44e12eae
hellfishg <hellfish.g@protonmail.com>
2023-02-03 03:08:05 -0300

    Uso de contextos sobre templates tags

--- src/pages/views.py
+++ src/pages/views.py
@@ -8,7 +8,11 @@ def home_view(request, *arg, **kwargs):
     return render(request, "home.html", {})
   
 def biblos_view(request, *arg, **kwargs):
-    return render(request, "biblos.html", {})
+    context_data = {
+        "user" : "Anonimo",
+        "age" : "23"
+    }
+    return render(request, "biblos.html", context_data)
 
 def contac_view(request, *arg, **kwargs):
     return HttpResponse("<h1>PAGINA DE INICIO!</h1>")
