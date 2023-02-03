4714ddada3af8107db44274ad957c4d6601fa13a
hellfishg <hellfish.g@protonmail.com>
2023-02-02 22:13:05 -0300

    Modificacion funciona de Modelo Productos

--- src/products/models.py
+++ src/products/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    name = models.TextField()
+    id = models.AutoField(auto_created=True)
+    name = models.TextField(max_length=120, blank=False)
     description = models.TextField()
-    price = models.TextField()
+    price = models.DecimalField(max_digits=2, max_length=10000)
+    summary = models.BooleanField(default=True)
