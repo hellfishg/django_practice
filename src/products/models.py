ab66d758bf6b1862f5d2d25b0266cb7266fccf07
hellfishg <hellfish.g@protonmail.com>
2023-02-02 22:25:55 -0300

    Creacion de el backoffice

--- src/products/models.py
+++ src/products/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    id = models.AutoField(auto_created=True)
+    id = models.AutoField(primary_key=True)
     name = models.TextField(max_length=120, blank=False)
     description = models.TextField()
-    price = models.DecimalField(max_digits=2, max_length=10000)
+    price = models.DecimalField(decimal_places=2, max_digits=10000)
     summary = models.BooleanField(default=True)
